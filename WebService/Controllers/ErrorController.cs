ac3fbf8df6346c19d3762b42201212305510c2ce
Djaba00 <shipelenko9@gmail.com>
2023-11-09 22:43:50 +0500

    глобальный обработчик ошибок

---
+++ WebService/Controllers/ErrorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.WebService.Controllers
+{
+    [Route("Error")]
+    public class ErrorController : Controller
+	{
+		public ErrorController()
+		{
+		}
+
+		[Route("403")]
+		public IActionResult Forbidden()
+		{
+			return View("Forbidden");
+		}
+
+        [Route("404")]
+        public IActionResult NotFound()
+		{
+			return View("NotFound");
+		}
+
+		[Route("752")]
+		public IActionResult SomethingWentWrong()
+		{
+			return View("SomethingWentWrong");
+		}
+	}
+}
+
