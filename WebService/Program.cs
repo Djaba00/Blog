d19d21ddb7053f8e453fafd567385b445538eb84
Djaba00 <shipelenko9@gmail.com>
2023-11-09 21:13:09 +0500

    настройка и конфигурация NLog

--- WebService/Program.cs
+++ WebService/Program.cs
@@ -1,5 +1,5 @@
-﻿
-using Microsoft.AspNetCore.Hosting;
+﻿using NLog.Extensions.Logging;
+using NLog.Web;
 
 namespace Blog.WebService
 {
@@ -13,9 +13,18 @@ namespace Blog.WebService
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddEventSourceLogger();
+                    logging.AddNLog();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+                
     }
 }
