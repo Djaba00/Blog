f73b7b0045359be1d0078d8d37b7e12889177458
Djaba00 <shipelenko9@gmail.com>
2023-11-21 13:49:16 +0500

    Рефакторинг

--- Blog.BLL/Configurations/ServicesBLLModule.cs
+++ Blog.BLL/Configurations/ServicesBLLModule.cs
@@ -40,7 +40,8 @@ namespace Blog.BLL.Configurations
         {
             services.AddDbContext<DataContext>(
                 options => options.UseSqlite(connectionString,
-                opts => opts.MigrationsAssembly("Blog.DAL")))
+                opts => opts.MigrationsAssembly("Blog.DAL"))
+                .EnableSensitiveDataLogging())
                 .AddIdentity<UserAccount, Role>(options =>
                 {
                     options.Password.RequiredLength = 5;
