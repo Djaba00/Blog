11d3e7cecf5f2115f3f3d99e0b4bd177b73927fb
Djaba00 <shipelenko9@gmail.com>
2023-11-28 15:36:26 +0500

    Рефакторинг

--- WebService/ViewModels/Account/AccountViewModel.cs
+++ WebService/ViewModels/Account/AccountViewModel.cs
@@ -13,6 +13,8 @@ namespace Blog.WebService.ViewModels.Account
         public string UserName { get; set; }
         public string Email { get; set; }
 
+        public string Password { get; set; }
+
         public UserProfileViewModel Profile { get; set; }
 
         public List<AccountRoleViewModel> Roles { get; set; }
