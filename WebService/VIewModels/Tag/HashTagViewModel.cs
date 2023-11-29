08baaa82ab634402c3afef1789cb5b155cc3aebc
Timofey <94359448+Djaba00@users.noreply.github.com>
2023-11-30 00:44:36 +0600

    Delete WebService/VIewModels directory

--- WebService/VIewModels/Tag/HashTagViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Blog.WebService.ViewModels.Tag
-{
-    public class HashTagViewModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public bool Selected { get; set; }
-    }
-}
